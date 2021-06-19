a58f3c8d0048606c9b92a50f41766b8147e21cb0
kimmr <kimmr@vcu.edu>
2021-06-19 18:20:14 -0400

    Revert "Revert "Build: Entry for new expense""
    This reverts commit 5999e6a7d66d127c0b4e25aa8e5ef42a73da390a.

--- src/components/Expenses/ExpenseComponents.js
+++ src/components/Expenses/ExpenseComponents.js
@@ -1,32 +1,49 @@
+import React, { useState } from 'react';
+
 import ExpenseItem from './ExpenseItem';
+import ExpenseFilter from './ExpenseFilter';
 import Card from '../UI/Card';
-import './ExpenseComponents.css'
+import './ExpenseComponents.css';
 
 const ExpenseComponents = (props) => {
-  return (
-    <Card className="expenses">
-      <ExpenseItem
-        title={props.item[0].title}
-        price={props.item[0].price}
-        date={props.item[0].date}
-      />
-      <ExpenseItem
-        title={props.item[1].title}
-        price={props.item[1].price}
-        date={props.item[1].date}
-      />
-      <ExpenseItem
-        title={props.item[2].title}
-        price={props.item[2].price}
-        date={props.item[2].date}
-      />
-      <ExpenseItem
-        title={props.item[3].title}
-        price={props.item[3].price}
-        date={props.item[3].date}
-      />
-    </Card>
+  const [filteredYear, setFilteredYear] = useState(
+    new Date().getFullYear().toString()
   );
-}
+
+  const yearFilterHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
+  return (
+    <div>
+      <Card className="expenses">
+        <ExpenseFilter
+          selectedYear={filteredYear}
+          onSendAYear={yearFilterHandler}
+        />
+        <ExpenseItem
+          title={props.item[0].title}
+          price={props.item[0].price}
+          date={props.item[0].date}
+        />
+        <ExpenseItem
+          title={props.item[1].title}
+          price={props.item[1].price}
+          date={props.item[1].date}
+        />
+        <ExpenseItem
+          title={props.item[2].title}
+          price={props.item[2].price}
+          date={props.item[2].date}
+        />
+        <ExpenseItem
+          title={props.item[3].title}
+          price={props.item[3].price}
+          date={props.item[3].date}
+        />
+      </Card>
+    </div>
+  );
+};
 
 export default ExpenseComponents;
